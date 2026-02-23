d9b5891e094ed4dbd2379ae9047c1a399d1440ca
Seth Randall <sethjrandall@gmail.com>
2026-02-23 10:31:33 -0500

    Update for local timezone

--- src/app/home-client.tsx
+++ src/app/home-client.tsx
@@ -4,7 +4,13 @@ import { get } from "http";
 import { useState } from "react";
 
 export default function Home() {
-  const today = new Date().toISOString().split("T")[0];
+  const now = new Date();
+
+  const today = [
+    now.getFullYear(),
+    String(now.getMonth() + 1).padStart(2, "0"),
+    String(now.getDate()).padStart(2, "0"),
+  ].join("-");
 
   const [totalCheckIns, setTotalCheckIns] = useState<number>(() => {
     if (typeof window === "undefined") return 0;
